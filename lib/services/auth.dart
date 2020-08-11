6d5c84c65c6f7deeec3a2f1938d350bdf2154b21
Lâm Nguyễn Hữu <huulamthcsyl@gmail.com>
2020-08-11 16:57:20 +0700

    Sign In

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -5,28 +5,34 @@ final FirebaseAuth auth = FirebaseAuth.instance;
 final db = Firestore.instance;
 
 Future<String> handleSignInEmail(String email, String password) async {
-  AuthResult result = await auth.signInWithEmailAndPassword(email: email, password: password);
-  final FirebaseUser user = result.user;
+  try {
+    AuthResult result = await auth.signInWithEmailAndPassword(email: email, password: password);
+    final FirebaseUser user = result.user;
 
-  assert(user != null);
-  assert(await user.getIdToken() != null);
+    assert(user != null);
+    assert(await user.getIdToken() != null);
 
-  final FirebaseUser currentUser = await auth.currentUser();
-  assert(user.uid == currentUser.uid);
+    final FirebaseUser currentUser = await auth.currentUser();
+    assert(user.uid == currentUser.uid);
 
-  await db.collection('account').document(user.uid).get().then((value) {return value;});
-
-  return null;
+    return db.collection('account').document(user.uid).collection('type').toString();
+  } catch (e) {
+    print(e);
+  }
 }
 
 Future<FirebaseUser> handleSignUp(email, password, type) async {
-  AuthResult result = await auth.createUserWithEmailAndPassword(email: email, password: password);
-  final FirebaseUser user = result.user;
+  try{
+    AuthResult result = await auth.createUserWithEmailAndPassword(email: email, password: password);
+    final FirebaseUser user = result.user;
 
-  assert(user != null);
-  assert(await user.getIdToken() != null);
+    assert(user != null);
+    assert(await user.getIdToken() != null);
 
-  await db.collection('account').document(user.uid).setData({'type': type});
+    await db.collection('account').document(user.uid).setData({'type': type});
 
-  return user;
+    return user;
+  } catch (e) {
+    print(e);
+  }
 }
